223239844aa917e0be33c732fc3dfee99800cb4b
marston <marston@jackallabs.io>
2025-04-19 21:32:34 -0400

    favicon stuff

--- scripts/generate-favicon.js
+++ scripts/generate-favicon.js
@@ -1,5 +1,6 @@
 const sharp = require('sharp');
-const fs = require('fs');
+const pngToIco = require('png-to-ico');
+const fs = require('fs').promises;
 const path = require('path');
 
 // Create a 32x32 black square
@@ -10,14 +11,26 @@ const blackSquare = Buffer.from(
     </svg>`
 );
 
-// Generate favicon.ico
-sharp(blackSquare)
-    .resize(size, size)
-    .toFormat('ico')
-    .toFile('favicon.ico')
-    .then(() => {
+async function generateFavicon() {
+    try {
+        // First generate PNG
+        await sharp(blackSquare)
+            .resize(size, size)
+            .png()
+            .toFile('temp-favicon.png');
+
+        // Convert PNG to ICO
+        const buf = await pngToIco('temp-favicon.png');
+        await fs.writeFile('favicon.ico', buf);
+
+        // Clean up temporary PNG
+        await fs.unlink('temp-favicon.png');
+
         console.log('favicon.ico generated successfully');
-    })
-    .catch(err => {
+    } catch (err) {
         console.error('Error generating favicon:', err);
-    }); 
+        process.exit(1);
+    }
+}
+
+generateFavicon(); 
